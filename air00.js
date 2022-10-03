24e782587bc529f8b4a07991168e0a2c09ac5539
Hugo Calimache <kalim1205@gmail.com>
2022-10-03 17:25:50 +0200

    Mise à jour

--- air00.js
+++ air00.js
@@ -1,23 +1,35 @@
-function splitMaison(arrayOfWord, separateur) {
+function splitMaison(arrayOfWord, separateurs) {
 
-	let arr = [''];
-	let index = 0;
-	arrayOfWord = arrayOfWord[0];
+	// On vérifie les arguments
+	if (arrayOfWord.length !== 1) {
+		console.log('error');
 
-	for (let i = 0; i < arrayOfWord.length; i++) {
-		if (arrayOfWord.charAt(i).includes(separateur.charAt(0, 1, 2)) === true) {
-			index++;
-			arr[index] = [''];
-		} else {
-			arr[index] += arrayOfWord.charAt(i);
+	} else {
+
+		// On initialise le tableau de retour et son compteur d'index
+		// On en profite pour modifier arrayOfWord (array) en strACut (str)
+		let arr = [''];
+		let index = 0;
+		strACut = arrayOfWord[0];
+
+		// On parcourt la str et vérifie si le caractère est contenu dans separateurs
+		for (let i = 0; i < strACut.length; i++) {
+			if (strACut.charAt(i).includes(separateurs.charAt(0, 1, 2)) === true) {
+				// On écrit rien mais on augmente la valeur d'index
+				index++;
+				// Surtout on créé la 'case' du tableau, sinon on récupère 'undefinedles' ou 'undefinedgars'
+				arr[index] = [''];
+			} else {
+				arr[index] += strACut.charAt(i);
+			}
+
 		}
 
+		// On affiche chaque case de notre array
+		for ( let i = 0; i !== arr.length; i++ ) {
+	      console.log(arr[i]);
+	    }
 	}
-
-	for ( let i = 0; i !== arr.length; i++ ) {
-      console.log(arr[i]);
-    }
-
 }
 
 return splitMaison(process.argv.slice(2), " \n\t");
