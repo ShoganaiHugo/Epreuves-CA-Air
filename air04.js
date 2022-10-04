0db410b301d0b4d030ab2f8fc974fe950547e2f6
Hugo Calimache <kalim1205@gmail.com>
2022-10-04 14:25:49 +0200

    Ajout 04 et 05

---
+++ air04.js
@@ -0,0 +1,27 @@
+function oneTime(arrayOfWord) {
+
+	// On vérifie les arguments
+	if (arrayOfWord.length !== 1) {
+		return 'error';
+
+	} else {
+
+		// On s'assure de manipuler une str et on créé une str de retour
+		arrayOfWord = arrayOfWord[0];
+		let strResult = '';
+
+		// Si i === i++ alors on passe à la suite, sinon on écrit le caractère dans la nouvelle str
+		for (let i = 0; i < arrayOfWord.length; i++) {
+			if (arrayOfWord.charAt(i) === arrayOfWord.charAt(i + 1)) {
+				continue;
+			} else {
+				strResult += arrayOfWord[i];
+			}
+		}
+
+		return strResult;
+		
+	}
+}
+
+console.log(oneTime(process.argv.slice(2)));
