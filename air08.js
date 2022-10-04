e5f5e6dce6a911721493e0e5e5079d86ae8e30a0
Hugo Calimache <kalim1205@gmail.com>
2022-10-04 15:42:57 +0200

    Ajout 08

---
+++ air08.js
@@ -0,0 +1,26 @@
+function rotateLeft(arrayToRotate) {
+	
+	// On vérifie les arguments
+	if (arrayToRotate.length < 2) {
+		return 'error';
+
+	} else {
+
+		// On copie le tableau dans le tableau de retour et prepare une var temporaire
+		let arrayResult = arrayToRotate;
+		let tmp = '';
+
+		// On fait le tour du array et chaque fois on inverse i et i+1
+		for (let i = 0; i < arrayResult.length - 1; i++) {
+			tmp = arrayResult[i];
+			arrayResult[i] = arrayResult[i + 1];
+			arrayResult[i + 1] = tmp;
+		}
+
+		// On affiche le résultat au bon format
+		return arrayResult.join(', ');
+
+	}
+}
+
+console.log(rotateLeft(process.argv.slice(2)));
